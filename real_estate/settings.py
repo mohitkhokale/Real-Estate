d85e57160144efa9d0620d8f2eabc934634de831
mohitkhokale <mohit041995@gmail.com>
2022-04-02 16:36:31 +0530

    Code Updated with DRF

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*5-3&646hi4ytd6mvjj%i&ko8@ci_ly!6lj^q-4)@ywkcz-i0)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
